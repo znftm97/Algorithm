2f78f81cc592e381982a5966e8fa390daa227de7
jihoon <znftm97@naver.com>
2021-03-30 19:27:19 +0900

    백준 2751

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,22 +3,28 @@ package com.company;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
+        List<Integer> list = new ArrayList<>();
 
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+            list.add(Integer.parseInt(br.readLine()));
         }
 
-        Arrays.sort(arr);
-        for (int a : arr) {
-            System.out.println(a);
+        Collections.sort(list);
+
+        for (int a : list) {
+            sb.append(a).append("\n");
         }
+        System.out.println(sb);
+
     }
 }
