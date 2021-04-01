e5ed270fdfc4494968e208540a53edcf1984ef4f
jihoon <znftm97@naver.com>
2021-04-01 18:05:11 +0900

    문자열2

---
+++ src/com/company/백준/문자열/_2941.java
@@ -0,0 +1,20 @@
+package com.company.백준.문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _2941 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] arr = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+        String text = br.readLine();
+
+        for (int i = 0; i < arr.length; i++) {
+            text = text.replaceAll(arr[i], "@");
+        }
+
+        System.out.println(text.length());
+    }
+}
---
+++ src/com/company/백준/문자열/_5622.java
@@ -0,0 +1,24 @@
+package com.company.백준.문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _5622 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] arr = {"", "", "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"};
+        String text = br.readLine();
+
+        int sum = 0;
+        for (int i = 0; i < text.length(); i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[j].contains(String.valueOf(text.charAt(i)))) {
+                    sum += j;
+                }
+            }
+        }
+        System.out.println(sum+text.length());
+    }
+}
