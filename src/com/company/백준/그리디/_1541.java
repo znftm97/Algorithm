8e86f90e53374d53fdd84ffcfb5d55cc37ed96be
jihoon <znftm97@naver.com>
2021-04-05 21:58:36 +0900

    그리디

---
+++ src/com/company/백준/그리디/_1541.java
@@ -0,0 +1,33 @@
+package com.company.백준.그리디;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _1541 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), "-");
+
+        int sum = 111111;
+        while (st.hasMoreTokens()) {
+            int tmp = 0;
+
+            StringTokenizer st2 = new StringTokenizer(st.nextToken(), "+");
+
+            while (st2.hasMoreTokens()) {
+                tmp += Integer.parseInt(st2.nextToken());
+            }
+
+            if (sum == 111111) {
+                sum = tmp;
+            } else {
+                sum -= tmp;
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
