374716da535d7dc8e567b841a2593f7b1ecb1bd0
jihoon <znftm97@naver.com>
2021-05-18 23:42:24 +0900

    프로그래머스 음양더하기, 소수만들기

---
+++ src/com/company/프로그래머스/LV1/음양더하기.java
@@ -0,0 +1,17 @@
+package com.company.프로그래머스.LV1;
+
+public class 음양더하기 {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+
+        for (int i = 0; i < absolutes.length; i++) {
+            if (signs[i]) {
+                answer += absolutes[i];
+            } else {
+                answer -= absolutes[i];
+            }
+        }
+
+        return answer;
+    }
+}
