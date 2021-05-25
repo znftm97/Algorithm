858c34936ef9cb15f4da49adfc4f4bd453f4f88a
jihoon <znftm97@naver.com>
2021-05-25 23:32:53 +0900

    프로그래머스 정렬, 큐

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,9 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        String a = "AAA";
+        String b = "BBB";
+
+        System.out.println(a.compareTo(b));
     }
 }
