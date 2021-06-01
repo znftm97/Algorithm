f01f8a426817d9ec6f1a592be5c8958f58229f9c
jihoon <znftm97@naver.com>
2021-06-01 22:17:23 +0900

    프로그래머스 문제 풀이

---
+++ src/com/company/프로그래머스/Etc/문자열내임차순으로배치하기.java
@@ -0,0 +1,20 @@
+package com.company.프로그래머스.Etc;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class 문자열내임차순으로배치하기 {
+    public String solution(String s) {
+        String answer = "";
+        String[] arr = s.split("");
+
+        Arrays.sort(arr);
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        for (String str : arr) {
+            answer += str;
+        }
+
+        return answer;
+    }
+}
