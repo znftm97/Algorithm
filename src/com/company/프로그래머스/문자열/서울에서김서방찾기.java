77c72f4bfb2fc24b0eb5dc90824f716e05b7d178
jihoon <znftm97@naver.com>
2021-06-02 16:32:50 +0900

    프로그래머스 문제풀이

---
+++ src/com/company/프로그래머스/문자열/서울에서김서방찾기.java
@@ -0,0 +1,18 @@
+package com.company.프로그래머스.문자열;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class 서울에서김서방찾기 {
+    public String solution(String[] seoul) {
+        int index = 0;
+        for (int i = 0; i < seoul.length; i++) {
+            if (seoul[i].equals("Kim")) {
+                index = i;
+                break;
+            }
+        }
+
+        return "김서방은 " + index + "에 있다";
+    }
+}
