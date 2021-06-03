9ba07353fe6b7436e9168fccfacf6acfcbe89d9f
jihoon <znftm97@naver.com>
2021-06-03 18:55:11 +0900

    프로그래머스 문제풀이

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,16 +4,5 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int count = 0;
-        int n = 5;
-
-        for (int i = 2; i <= n; i++) {
-            for (int j = 2; j < Math.sqrt(i); j++) {
-                if (i % j == 0) {
-                    break;
-                }
-            }
-            count++;
-        }
     }
 }
