5ba96e92fb2ffb029c921faf1c8cb4e031b74287
jihoon <znftm97@naver.com>
2021-06-09 19:10:05 +0900

    프로그래머스 문제 풀이

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,66 +1,46 @@
 package com.company;
 
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.Stack;
 
 public class Main {
-    static char[] character = {'A', 'C', 'F', 'J', 'M', 'N', 'R', 'T'};
-    static char[] position;
-    static boolean[] check;
-    static int ans;
-
     public static void main(String[] args) throws IOException {
-        int n = 2;
-        String[] data = {"N~F=0", "R~T>2"};
-        position = new char[8];
-        check = new boolean[8];
-        ans = 0;
+        int a= 2;
+        int b= 3;
+        int n= 8;
+        int count = 1;
+        Stack<Integer> st = new Stack<>();
+        a -= 1;
+        b -= 1;
 
-        backtracking(0, n, data);
-    }
-
-    static void backtracking(int idx, int n, String[] data){
-        if(idx == 8){
-            if(isPossible(n, data)){
-                ans++;
+        while (true) {
+            if (0 == a || 0 == b) {
+                st.add(1);
+            } else {
+                st.add(0);
             }
-            return;
+
+            for (int i = 1; i < n; i++) {
+                if (st.peek() == 1 && (i == a || i == b) && i%2!=0) {
+                        System.out.println(count);
+                        return;
+                    }
+
+
+                if (i == a || i == b) {
+                    st.add(1);
+                } else {
+                    st.add(0);
+                }
+            }
+
+            st.clear();
+            count++;
+            n = n/2;
+            a = a/2;
+            b = b/2;
         }
 
-        for(int i = 0 ; i < 8 ; ++i){
-            if(!check[i]){
-                check[i] = true;
-                position[idx] = character[i];
-                backtracking(idx + 1, n, data);
-                check[i] = false;
-            }
-        }
-    }
-
-    static boolean isPossible(int n, String[] data){
-        for(int i = 0 ; i < n ; ++i){
-            char[] condition = data[i].toCharArray();
-            int from = 0, to = 0, gap = 0, cond = condition[4] - '0';
-
-            for(int j = 0 ; j < 8 ; ++j){
-                if(position[j] == condition[0]) from = j;
-                if(position[j] == condition[2]) to = j;
-            }
-
-            gap = Math.abs(from - to) - 1;
-            switch(condition[3]){
-                case '=':
-                    if(gap != cond) return false;
-                    break;
-                case '<':
-                    if(gap >= cond) return false;
-                    break;
-                case '>':
-                    if(gap <= cond) return false;
-                    break;
-            }
-        }
-        return true;
     }
 }
 
---
+++ src/com/company/프로그래머스/Etc/예상대진표.java
@@ -0,0 +1,43 @@
+package com.company.프로그래머스.Etc;
+
+import java.util.Stack;
+
+public class 예상대진표 {
+    public int solution(int n, int a, int b) {
+        int count = 1;
+        Stack<Integer> st = new Stack<>();
+        a -= 1;
+        b -= 1; // 배열의 인덱스와 맞추기 위해
+
+        while (true) {
+            /*빈 스택에 peek 하면 오류나므로 처음은 그냥 값 넣어줌*/
+            if (0 == a || 0 == b) {
+                st.add(1);
+            } else {
+                st.add(0);
+            }
+
+            for (int i = 1; i < n; i++) {
+                /*이전에 1값이 들어왔고 && 현재 1값이 들어가야하고 && i가 홀수라면 둘이 만난다.
+                * i가 홀수일 때만 해야하는 이유는 0ab0 처럼 a와 b가 붙어있어도 서로 같은 대진이 아니기 때문에 앞에 숫자 즉 홀수일때만 검사해야한다.
+                * */
+                if (st.peek() == 1 && (i == a || i == b) && i%2!=0) {
+                    return count;
+                }
+
+                if (i == a || i == b) {
+                    st.add(1);
+                } else {
+                    st.add(0);
+                }
+            }
+
+            /*안만났다면 스택을 비우고, 카운트 증가, 모든 숫자 나누기 2*/
+            st.clear();
+            count++;
+            n = n/2;
+            a = a/2;
+            b = b/2;
+        }
+    }
+}
