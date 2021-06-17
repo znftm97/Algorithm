648b8d42a530b2e2e5f72d049e3007f2b617bc9d
jihoon <znftm97@naver.com>
2021-06-17 22:28:37 +0900

    백준 문제풀이

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,69 +1,69 @@
 package com.company;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Pattern;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-    private static final Pattern IDENTIFY = Pattern.compile("[A-Z][A-Z]");
+    static int[][] map;
+    static boolean visit[][];
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int N;
+    static int M;
 
-    public static void main(String[] args) throws IOException {
-        String str1 = "FRANCE";
-        String str2 = "french";
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-        List<String> list1 = new ArrayList<>();
-        List<String> list2 = new ArrayList<>();
-        double intersectionCount = 0; // 교집합 개수
-        double unionCount = 0; // 합집합 개수
-        int answer = 0;
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+        visit = new boolean[N][M];
 
-        str1 = str1.toUpperCase();
-        str2 = str2.toUpperCase();
+        for (int i = 0; i < N; i++) {
+            String s = br.readLine();
 
-        /*두 글자씩 추출*/
-        list1 = extract(str1);
-        list2 = extract(str2);
-
-
-        for(String s : list1){
-            if(list2.remove(s)){
-                intersectionCount++;
+            for (int j = 0; j < M; j++) {
+                map[i][j] = s.charAt(j) - '0';
             }
-            unionCount++;
         }
 
-        for(String s : list2){
-            unionCount++;
-        }
+        BFS(0, 0);
 
-        System.out.println(intersectionCount / unionCount);
-        answer = (int) Math.floor((intersectionCount / unionCount) * 65536); // int형으로 캐스팅해서 소수값 버리기
-        if (answer == 0) { // 일치하는 요소가 없는 경우
-            System.out.println("65536");
-        }
-
-        System.out.println(answer);
+        System.out.println(map[N-1][M-1]);
     }
 
-    static List<String> extract(String s) {
-        List<String> list = new ArrayList<>();
-        int before = 0;
-        int after = 1;
+    static void BFS(int x, int y) {
+        Queue<Integer> qx = new LinkedList<>();
+        Queue<Integer> qy = new LinkedList<>();
+        qx.add(x);
+        qy.add(y);
 
-        while (true) {
-            if (after == s.length()) { // 문자열 끝까지 다 탐색했을 때
-                return list;
+        while (!qx.isEmpty() && !qy.isEmpty()) {
+            x = qx.poll();
+            y = qy.poll();
+            visit[x][y] = true;
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (nx >= 0 && ny >= 0 && nx < N && ny < M && map[nx][ny] == 1) {
+                    if (!visit[nx][ny]) {
+                        qx.add(nx);
+                        qy.add(ny);
+                        visit[nx][ny] = true;
+                        map[nx][ny] = map[x][y] + 1;
+                    }
+                }
             }
-
-            String createStr = String.valueOf(s.charAt(before)) + String.valueOf(s.charAt(after));
-            if(IDENTIFY.matcher(createStr).matches()){
-                list.add(createStr);
-            }
-
-            before++;
-            after++;
         }
     }
+
+
+
 }
 
---
+++ src/com/company/백준/bfs_dfs/_11724.java
@@ -0,0 +1,63 @@
+package com.company.백준.bfs_dfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class _11724 {
+    static ArrayList<Integer>[] list;
+    static boolean[] check;
+    static int count;
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        list = new ArrayList[N + 1];
+        check = new boolean[N + 1];
+
+        for (int i = 1; i < N+1; i++) {
+            list[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            list[x].add(y);
+            list[y].add(x);
+        }
+
+        count = 0;
+        for (int i = 1; i <= N; i++) {
+            if (check[i] == true) {
+                continue;
+            }
+
+            DFS(i);
+            count++;
+        }
+
+        System.out.println(count);
+    }
+
+    static void DFS(int start) {
+        if (check[start] == true) {
+            return;
+        }
+        check[start] = true;
+
+        for (int x : list[start]) {
+            if (!check[x]) {
+                DFS(x);
+            }
+        }
+    }
+}
