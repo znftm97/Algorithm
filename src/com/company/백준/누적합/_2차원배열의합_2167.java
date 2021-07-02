543e5deda3c006ab7ed3acda1205dc822aa0e415
jihoon <znftm97@naver.com>
2021-07-02 23:56:53 +0900

    누적합 알고리즘 풀이2

--- src/com/company/백준/누적합/_2차원배열의합_2167.java
+++ src/com/company/백준/누적합/_2차원배열의합_2167.java
@@ -1,46 +1,9 @@
 package com.company.백준.누적합;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class _2차원배열의합_2167 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int[][] arr = new int[N][M];
-
-        /*배열에 값 입력*/
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-
-            for (int j = 0; j < M; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        /*누적 합*/
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M - 1; j++) { // M에서 1을 빼줘야함, j가 다음 요소를 탐색하므로
-                arr[i][j+1] = arr[i][j+1] + arr[i][j];
-            }
-            if(i!=N-1){ // i가 다음 행을 탐색하므로 마지막 행일때는 수행하면 안됨
-                arr[i+1][0] = arr[i][M-1] + arr[i+1][0]; // 한 행 누적합이 끝나면 다음행의 첫번째 열 값과 더해줘야함
-            }
-        }
-
-        int K = Integer.parseInt(st.nextToken());
-        for (int i = 0; i < K; i++) {
-            st = new StringTokenizer(br.readLine());
-
-        }
-
 
     }
-
 }
--- src/com/company/백준/누적합/_2차원배열의합_2167_Custom.java
+++ src/com/company/백준/누적합/_2차원배열의합_2167_Custom.java
@@ -1,11 +1,17 @@
-package com.company;
+package com.company.백준.누적합;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+/*문제 설명이 친절하지 못하다. 아니 설명이없다 출력값을 보고 유추해야한다.
+* 1,1 2,3 이 주어지면 1,1 1,2 ... 2,2 2,3 의 값을 모두 더하는건데
+* 1,3 2,3 이 주어지면 1,3 2,1 2,2 2,3 의 값을 모두 더하는게 아니라 1,3 2,3의 값만 더하는것이다.
+* 직관적으로 마우스 드래그를 생각하면 될듯하다.
+* 이 클래스는 원래 내가 생각한 모두 더하는 방식을 기준으로 풀이한 방법이다.
+* */
+public class _2차원배열의합_2167_Custom {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
@@ -40,7 +46,5 @@ public class Main {
 
         System.out.println(sb.toString());
     }
+
 }
-
-
-
