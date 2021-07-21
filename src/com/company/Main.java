7dc61a46919bb17e772e819e2d75fd6a550c2ff0
jihoon <znftm97@naver.com>
2021-07-21 21:56:22 +0900

    투포인터 문제풀이

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,50 +7,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
-        String origin = br.readLine();
-        String pattern = br.readLine();
-
-        int[] pi = getPi(pattern);
-        kmp(pi, origin, pattern);
-    }
-
-    static int[] getPi(String pattern){
-        int j = 0;
-        int[] pi = new int[pattern.length()];
-
-        for (int i = 1; i < pattern.length(); i++) {
-            while (j > 0 && pattern.charAt(i) != pattern.charAt(j)) {
-                j = pi[j - 1];
-            }
-
-            if (pattern.charAt(i) == pattern.charAt(j)) {
-                pi[i] = ++j;
-            }
-        }
-
-        return pi;
-    }
-
-    static void kmp(int[] pi, String origin, String pattern) {
-        int j = 0;
-        for (int i = 0; i < origin.length(); i++) {
-            while (j > 0 && origin.charAt(i) != pattern.charAt(j)) {
-                j = pi[j - 1];
-            }
-            if (origin.charAt(i) == pattern.charAt(j)) {
-                if (j == pattern.length() - 1) {
-                    System.out.println(1);
-                    return;
-                } else {
-                    j++;
-                }
-            }
-        }
-
-        System.out.println(0);
     }
 }
 
