f3d5a3d2d7bd789b16c2325e5c45195d50a2814a
jihoon <znftm97@naver.com>
2021-07-22 23:05:33 +0900

    투 포인터 알고리즘 문제풀이

--- src/com/company/백준/투포인터/부분합_1806.java
+++ src/com/company/백준/투포인터/부분합_1806.java
@@ -5,36 +5,40 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class 수들의합2 {
+public class 부분합_1806 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
         int[] arr = new int[N];
-        int start = 0, end = 0;
-        int count = 0;
-        int sum = 0;
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
+        int start = 0;
+        int end = 0;
+        int shortest = 100001;
+        int sum = 0;
+
         while (true) {
-            if (sum >= M) {
+            if (sum >= S) {
+                shortest = Math.min(shortest, end - start);
                 sum -= arr[start++];
             } else if (end == N) {
                 break;
             } else {
                 sum += arr[end++];
             }
-            if (sum == M) {
-                count++;
-            }
         }
 
-        System.out.println(count);
+        if (shortest == 100001) {
+            System.out.println(0);
+        } else {
+            System.out.println(shortest);
+        }
     }
 }
