3b0aed250b22578c050741e2765d3e362a5a28a9
jihoon <znftm97@naver.com>
2021-07-23 20:59:06 +0900

    투포인터 알고리즘 문제풀이

---
+++ src/com/company/백준/투포인터/배열합치기_11728.java
@@ -0,0 +1,40 @@
+package com.company.백준.투포인터;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class 배열합치기_11728 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        List<Integer> list = new ArrayList<>();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+
+        Collections.sort(list);
+
+        for (int a : list) {
+            sb.append(a).append(" ");
+        }
+
+        System.out.println(sb.toString());
+    }
+}
