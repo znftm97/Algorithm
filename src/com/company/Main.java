615518c69bc43dbb02844ff5437086b7d4b42e71
jihoon <znftm97@naver.com>
2021-08-07 15:18:04 +0900

    위상정렬

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,70 +1,9 @@
 package com.company;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
 
 public class Main {
-    static int n;
-    static int m;
-
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
-
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        int[] indegree = new int[n + 1];
-        int[][] graph = new int[n+1][n+1];
-
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int count = Integer.parseInt(st.nextToken());
-            int point1 = Integer.parseInt(st.nextToken());
-
-            for (int j = 0; j < count-1; j++) {
-                int point2 = Integer.parseInt(st.nextToken());
-                if (graph[point1][point2] == 0) {
-                    graph[point1][point2] = 1;
-                    indegree[point2]++;
-                }
-
-                point1 = point2;
-            }
-        }
-
-        Queue<Integer> q = new LinkedList<>();
-
-        for (int i = 1; i <= n; i++) {
-            if (indegree[i] == 0) {
-                q.add(i);
-            }
-        }
-
-        while (!q.isEmpty()) {
-            int node = q.poll();
-            sb.append(node).append("\n");
-
-            for (int i = 1; i <= n; i++) {
-                if (graph[node][i] == 1) {
-                    indegree[i]--;
-                    if (indegree[i] == 0) {
-                        q.add(i);
-                    }
-                }
-            }
-        }
-
-        for (int i = 1; i <= n; i++) {
-            if (indegree[i] != 0) {
-                sb.setLength(0);
-                sb.append(0);
-            }
-        }
-
-        System.out.println(sb);
     }
 }
 
---
+++ src/com/company/백준/위상정렬/작업_2056.java
@@ -0,0 +1,66 @@
+package com.company.백준.위상정렬;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class 작업_2056 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int[] indegree = new int[n + 1];
+        List<List<Integer>> graph = new ArrayList<>();
+        int[] workTime = new int[n + 1];
+        int[] result = new int[n+1];
+        int answer = 0;
+
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            workTime[i] = Integer.parseInt(st.nextToken());
+            int tmp = Integer.parseInt(st.nextToken());
+
+            if (tmp != 0) {
+                for (int j = 1; j <= tmp; j++) {
+                    graph.get(Integer.parseInt(st.nextToken())).add(i);
+                    indegree[i]++;
+                }
+            }
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 1; i <= n; i++) {
+            result[i] = workTime[i];
+
+            if (indegree[i] == 0) {
+                q.add(i);
+            }
+        }
+
+        while (!q.isEmpty()) {
+            int nowNode = q.poll();
+
+            for (int nextNode : graph.get(nowNode)) {
+                indegree[nextNode]--;
+                result[nextNode] = Math.max(result[nextNode], result[nowNode] + workTime[nextNode]);
+
+                if (indegree[nextNode] == 0) {
+                    q.add(nextNode);
+                }
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            answer = Math.max(answer, result[i]);
+        }
+
+        System.out.println(answer);
+    }
+}
