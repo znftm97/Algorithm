8e8e46901dae1e151009214dde4d0d3fa1db7c51
jihoon <znftm97@naver.com>
2021-08-10 22:03:51 +0900

    문자열

---
+++ src/com/company/백준/문자열/열개씩끊어출력하기_11721.java
@@ -0,0 +1,22 @@
+package com.company.백준.문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 열개씩끊어출력하기_11721 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String str = br.readLine();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (i % 10 == 0 && i != 0) {
+                sb.append("\n");
+            }
+            sb.append(str.charAt(i));
+        }
+        System.out.println(sb);
+
+    }
+}
