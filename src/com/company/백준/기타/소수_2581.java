193465aa19723167a9f3f9815fab21540ab057ae
jihoon <znftm97@naver.com>
2021-08-14 17:47:57 +0900

    실버5 문제풀이

---
+++ src/com/company/백준/기타/소수_2581.java
@@ -0,0 +1,54 @@
+package com.company.백준.기타;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 소수_2581 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int m = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        int min = Integer.MAX_VALUE;
+        int sum = 0;
+        boolean[] prime = new boolean[n+1];
+        prime[0] = true;
+        prime[1] = true;
+
+        for (int i = 2; i <= Math.sqrt(prime.length); i++) {
+            if(prime[i]) continue;
+
+            for (int j = i * i; j < prime.length; j += i) {
+                prime[j] = true;
+            }
+        }
+
+        for (int i = m; i <= n; i++) {
+            if (!prime[i]) {
+                sum+=i;
+
+                if (min == Integer.MAX_VALUE) {
+                    min = i;
+                }
+            }
+        }
+
+        if (sum == 0) {
+            System.out.println(-1);
+        } else {
+            System.out.println(sum);
+            System.out.println(min);
+        }
+
+        for (int i = m; i <= Math.sqrt(n); i++) {
+            if (prime[i]) {
+                continue;
+            }
+
+            for (int j = i * i; j < prime.length; j = j + i) {
+                prime[j] = true;
+            }
+        }
+    }
+}
