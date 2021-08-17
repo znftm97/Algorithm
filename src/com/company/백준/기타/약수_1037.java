9945021e9ac29c8ab4620e545d1c9dcce0646542
jihoon <znftm97@naver.com>
2021-08-17 14:59:50 +0900

    실5 문제풀이

---
+++ src/com/company/백준/기타/약수_1037.java
@@ -0,0 +1,26 @@
+package com.company.백준.기타;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class 약수_1037 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int count = Integer.parseInt(br.readLine());
+        int[] arr = new int[count];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < count; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        System.out.println(arr[0] * arr[count-1]);
+
+    }
+}
