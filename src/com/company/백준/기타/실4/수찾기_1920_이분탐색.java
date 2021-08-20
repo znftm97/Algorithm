f60a9b62c5fea0889479e1845544ecc32a3db938
jihoon <znftm97@naver.com>
2021-08-20 19:42:41 +0900

    실4 문제풀이

---
+++ src/com/company/백준/기타/실4/수찾기_1920_이분탐색.java
@@ -0,0 +1,35 @@
+package com.company.백준.기타.실4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class 수찾기_1920_이분탐색 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < M; i++) {
+            if(Arrays.binarySearch(arr, Integer.parseInt(st.nextToken())) >= 0) sb.append(1).append('\n');
+            else sb.append(0).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
