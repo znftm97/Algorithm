950ba90f7282c92c134859717356d6e2b0f4fcf8
jihoon <znftm97@naver.com>
2021-10-12 23:21:14 +0900

    누적합, 슬라이딩 윈도우

---
+++ src/com/company/백준/누적합/구간합구하기4_11659_다시풀기.java
@@ -0,0 +1,34 @@
+package com.company.백준.누적합;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 구간합구하기4_11659_다시풀기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(st.nextToken());
+        int testCase = Integer.parseInt(st.nextToken());
+        int[] accumulates = new int[N + 1];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            accumulates[i] = accumulates[i - 1] + Integer.parseInt(st.nextToken());
+        }
+
+        for (int t = 0; t < testCase; t++) {
+            st = new StringTokenizer(br.readLine());
+            int startIndex = Integer.parseInt(st.nextToken());
+            int endIndex = Integer.parseInt(st.nextToken());
+
+            int sum = accumulates[endIndex] - accumulates[startIndex - 1];
+            sb.append(sum).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
