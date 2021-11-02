7a60f148ff1a8bbdfc604c294dd3640e5a1bdd82
jihoon <znftm97@naver.com>
2021-11-02 22:40:31 +0900

    프로그래머스 문제풀이

--- src/com/company/백준/그리디/_1931.java
+++ src/com/company/백준/그리디/_1931.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class _1931 {
