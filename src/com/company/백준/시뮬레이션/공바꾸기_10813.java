17e2534d417e1c2cb98a4ae3a1d97ea0320b669c
jihoon <znftm97@naver.com>
2021-12-29 18:50:14 +0900

    시뮬레이션

---
+++ src/com/company/백준/시뮬레이션/공바꾸기_10813.java
@@ -0,0 +1,39 @@
+package com.company.백준.시뮬레이션;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 공바꾸기_10813 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] basket = new int[N + 1];
+
+        for (int i = 1; i < N + 1; i++) {
+            basket[i] = i;
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            int tmp = basket[x];
+            basket[x] = basket[y];
+            basket[y] = tmp;
+        }
+
+        for (int i = 1; i < N + 1; i++) {
+            sb.append(basket[i]).append(" ");
+        }
+        System.out.println(sb);
+        br.close();
+    }
+
+}
