3eccee48835e05c44a60626a684e866c0d161e2b
jihoon <znftm97@naver.com>
2022-01-26 19:26:50 +0900

    프로그래머스 LV2 문제 풀이

---
+++ src/com/company/프로그래머스/LV2/짝지어제거하기.java
@@ -0,0 +1,24 @@
+package com.company.프로그래머스.LV2;
+
+import java.util.Stack;
+
+public class 짝지어제거하기 {
+    public int solution(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (stack.size() == 0) {
+                stack.push(s.charAt(i));
+                continue;
+            }
+
+            char input = s.charAt(i);
+
+            if (input == stack.peek()) stack.pop();
+            else stack.push(input);
+        }
+
+        return stack.isEmpty() ? 1 : 0;
+    }
+
+}
