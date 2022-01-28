5e7fa56b9313a898268c7c6a3fdf8d10caceb719
jihoon <znftm97@naver.com>
2022-01-28 23:39:23 +0900

    프로그래머스 LV2 문제 풀이

---
+++ src/com/company/프로그래머스/LV2/전화번호목록_정렬.java
@@ -0,0 +1,15 @@
+package com.company.프로그래머스.LV2;
+
+import java.util.Arrays;
+
+public class 전화번호목록_정렬 {
+    public boolean solution(String[] phoneBook) {
+        Arrays.sort(phoneBook);
+
+        for (int i = 0; i < phoneBook.length - 1; i++) {
+            if(phoneBook[i+1].startsWith(phoneBook[i])) return false;
+        }
+
+        return true;
+    }
+}
