be5ea3c5257ead66ef37d514d7f21300d5c5619b
jihoon <znftm97@naver.com>
2022-02-26 15:27:17 +0900

    백트래킹

---
+++ src/com/company/프로그래머스/백트래킹/메뉴리뉴얼.java
@@ -0,0 +1,65 @@
+package com.company.프로그래머스.백트래킹;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class 메뉴리뉴얼 {
+
+    private static Map<String, Integer> foods;
+    private static int max;
+
+    public String[] solution(String[] orders, int[] course) {
+        PriorityQueue<String> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < course.length; i++) {
+            foods = new HashMap<>();
+            max = 0;
+
+            for (int j = 0; j < orders.length; j++) {
+                makeComb(0, course[i], "", 0, orders[j]);
+            }
+
+            for (String food : foods.keySet()) {
+                int recommendCnt = foods.get(food);
+
+                if (recommendCnt == max && max > 1) {
+                    pq.add(food);
+                }
+            }
+        }
+
+        String[] result = new String[pq.size()];
+        int index = 0;
+        while (!pq.isEmpty()) {
+            result[index++] = pq.poll();
+        }
+
+        return result;
+    }
+
+    private static void makeComb(int depth, int targetLen, String candidate, int index, String order) {
+        if (depth == targetLen) {
+            char[] chars = candidate.toCharArray();
+            Arrays.sort(chars);
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < chars.length; i++) {
+                sb.append(chars[i]);
+            }
+
+            candidate = sb.toString();
+            foods.put(candidate, foods.getOrDefault(candidate, 0) + 1);
+            max = Math.max(max, foods.get(candidate));
+
+            return;
+        }
+
+        for (int i = index; i < order.length(); i++) {
+            char c = order.charAt(i);
+            makeComb(depth + 1, targetLen, candidate + c, i + 1, order);
+        }
+    }
+
+}
---
+++ src/com/company/프로그래머스/백트래킹/소수만들기.java
@@ -0,0 +1,34 @@
+package com.company.프로그래머스.백트래킹;
+
+public class 소수만들기 {
+    private static int count = 0;
+
+    public int solution(int[] nums) {
+        dfs(0, 0, nums, 0);
+        return count;
+    }
+
+    private static void dfs(int depth, int num, int[] nums, int start) {
+        if(depth == 3){
+            if (isPrime(num)) count++;
+            return;
+        }
+
+        for (int i = start; i < nums.length; i++) {
+            num += nums[i];
+            dfs(depth + 1, num, nums, i + 1);
+            num -= nums[i];
+        }
+    }
+
+    private static boolean isPrime(int num) {
+        if(num < 2) return false;
+
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if(num % i == 0) return false;
+        }
+
+        return true;
+    }
+
+}
