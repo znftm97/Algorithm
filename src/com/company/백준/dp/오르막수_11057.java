71cdf5fdf66c0b455e6e5f3f8d759aa9190e3b70
jihoon <znftm97@naver.com>
2022-03-17 21:11:21 +0900

    DP 알고리즘

---
+++ src/com/company/백준/dp/오르막수_11057.java
@@ -0,0 +1,34 @@
+package com.company.백준.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 오르막수_11057 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] dp = new int[N + 1][10];
+
+        for (int i = 0; i < 10; i++) {
+            dp[1][i] = 1;
+        }
+
+        for (int i = 2; i <= N; i++) {
+            for (int j = 0; j < 10; j++) {
+                for (int k = 0; k <= j; k++) {
+                    dp[i][j] = (dp[i][j] + dp[i - 1][k]) % 10007;
+                }
+            }
+        }
+
+        int result = 0;
+        for (int i = 0; i < 10; i++) {
+            result += dp[N][i];
+        }
+
+        System.out.println(result % 10007);
+    }
+
+}
