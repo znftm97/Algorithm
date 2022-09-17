1bd9618f9f67854f6889b1c5efb5084029deef4d
jihoon <znftm97@naver.com>
2022-09-18 01:50:07 +0900

    백준 랜덤 문제 풀이

---
+++ src/com/company/v2/백준/기타_랜덤디펜스/절댓값힙_11286.java
@@ -0,0 +1,40 @@
+package com.company.v2.백준.기타_랜덤디펜스;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class 절댓값힙_11286 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> nums = new PriorityQueue<>((o1, o2) -> {
+            int n1 = Math.abs(o1);
+            int n2 = Math.abs(o2);
+
+            if(n1 == n2){
+                return o1 > o2 ? 1 : -1;
+            }
+
+            return n1 - n2;
+        });
+
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            if (num == 0) {
+                if (nums.isEmpty()) sb.append("0").append("\n");
+                else sb.append(nums.poll()).append("\n");
+            } else {
+                nums.add(num);
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+}
