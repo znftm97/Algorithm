a4c84a88da1d3ec6356ca5f7e65d7eeb18a787d5
jihoon <znftm97@naver.com>
2022-09-19 23:57:07 +0900

    SQL - solve LeetCode easy level

--- src/com/company/v2/릿코드/SQL/1000.sql
+++ src/com/company/v2/릿코드/SQL/1000.sql
@@ -11,6 +11,44 @@ select p.product_name as product_name,
 from sales s
 inner join product p on s.product_id = p.product_id
 
+#1069
+select product_id, sum(quantity) as total_quantity
+from sales s
+group by product_id;
+
+#1075
+select p.project_id, round(avg(e.experience_years), 2) as average_years
+from project p
+inner join employee e on p.employee_id = e.employee_id
+group by p.project_id
+
+#1076
+select project_id
+from project
+group by project_id
+having count(employee_id) =
+(
+    select count(employee_id)
+    from project
+    group by project_id
+    order by count(employee_id) desc
+    limit 1
+)
+
+#1082
+select seller_id
+from sales
+group by seller_id
+having sum(price) =
+(
+    select sum(price)
+    from sales
+    group by seller_id
+    order by sum(price) desc
+    limit 1
+);
+
+
 #1084
 select p.product_id, p.product_name
 from Product p
