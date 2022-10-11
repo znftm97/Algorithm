bee5bca3cf8a83a352214e878164a0673139a29d
jihoon <znftm97@naver.com>
2022-10-12 01:02:36 +0900

    SQL - solve LeetCode easy level

--- src/com/company/v2/릿코드/SQL/1400.sql
+++ src/com/company/v2/릿코드/SQL/1400.sql
@@ -5,6 +5,22 @@ left join Rides r on u.id = r.user_id
 group by r.user_id
 order by travelled_distance desc, name asc;
 
+#1421
+select q.id as id, q.year as year, ifnull(n.npv, 0) as npv
+from queries q
+    left join npv n on n.id = q.id and n.year = q.year
+
+#1435
+select "[0-5>" as bin, count(*) as total from Sessions where duration/60 < 5
+union
+select "[5-10>", count(*) from Sessions where duration/60 >= 5 and duration/60 < 10
+union
+select "[10-15>", count(*) from Sessions where duration/60 >= 10 and duration/60 < 15
+union
+select "15 or more", count(*) from Sessions where duration/60 >= 15;
+
+
+
 #1484
 select sell_date,
        count(distinct product) as num_sold,
