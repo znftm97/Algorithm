1bed341a8158b2c0f8545b2ff30d679ab2524417
jihoon <znftm97@naver.com>
2022-11-03 01:03:47 +0900

    SQL - solve LeetCode easy level

--- src/com/company/v2/릿코드/SQL/1600.sql
+++ src/com/company/v2/릿코드/SQL/1600.sql
@@ -9,6 +9,19 @@ where s.seller_name not in (
     )
 order by s.seller_name
 
+#1623
+select
+    a.student_name as member_A,
+    b.student_name as member_B,
+    c.student_name as member_C
+from SchoolA a, SchoolB b, SchoolC c
+where a.student_id != b.student_id and
+      b.student_id != c.student_id and
+      a.student_id != c.student_id and
+      a.student_name != b.student_name and
+      b.student_name != c.student_name and
+      a.student_name != c.student_name
+
 #1667
 select user_id, concat(substring(upper(name),1,1),
                        substring(lower(name),2)) as name
