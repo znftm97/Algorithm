d64551a747076788ff3081eb3c309b8422c2d1ec
jihoon <znftm97@naver.com>
2022-11-08 19:20:57 +0900

    SQL - solve LeetCode easy level

--- src/com/company/v2/릿코드/SQL/1600.sql
+++ src/com/company/v2/릿코드/SQL/1600.sql
@@ -54,6 +54,18 @@ select user_id, concat(substring(upper(name),1,1),
 from Users
 order by user_id;
 
+#1677
+SELECT
+    P.name AS name,
+    IFNULL(SUM(rest), 0) AS rest,
+    IFNULL(SUM(paid), 0) AS paid,
+    IFNULL(SUM(canceled), 0) AS canceled,
+    IFNULL(SUM(refunded), 0) AS refunded
+FROM Product P
+    LEFT JOIN Invoice I ON P.product_id = I.product_id
+GROUP BY P.product_id
+ORDER BY P.name
+
 #1693
 select date_id,
        make_name,
