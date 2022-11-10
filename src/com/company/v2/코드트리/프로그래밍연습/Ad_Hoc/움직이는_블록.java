cd3cbf3cd39c804aeb1be2a77f34d342eb068a1c
jihoon <znftm97@naver.com>
2022-11-11 00:32:06 +0900

    Algorithm - 코드트리 [프로그래밍 연습] 레벨2 - Ad-Hoc

---
+++ src/com/company/v2/코드트리/프로그래밍연습/Ad_Hoc/움직이는_블록.java
@@ -0,0 +1,29 @@
+package com.company.v2.코드트리.프로그래밍연습.Ad_Hoc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 움직이는_블록 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] blocks = new int[N];
+
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            blocks[i] = Integer.parseInt(br.readLine());
+            sum += blocks[i];
+        }
+
+        int avgBlock = sum / N;
+        int result = 0;
+        for (int block : blocks) {
+            result += Math.abs(block - avgBlock);
+        }
+
+        System.out.println(result / 2);
+    }
+
+}
