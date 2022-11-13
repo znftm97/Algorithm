8ecfd5305214921cdfbebd9c8b53d7159c85da59
jihoon <znftm97@naver.com>
2022-11-14 01:19:03 +0900

    SQL - solve LeetCode easy level

--- src/com/company/v2/릿코드/SQL/1800.sql
+++ src/com/company/v2/릿코드/SQL/1800.sql
@@ -1,3 +1,20 @@
+#1809 - 1
+SELECT session_id
+FROM Playback pb
+    LEFT JOIN Ads ad ON pb.customer_id = ad.customer_id
+    AND ad.timestamp BETWEEN start_time AND end_time
+WHERE ad.ad_id IS NULL
+
+#1809 - 2
+SELECT session_id
+FROM Playback
+WHERE session_id NOT IN (
+    SELECT distinct pb.session_id
+    FROM Playback pb
+         LEFT JOIN Ads ad ON pb.customer_id = ad.customer_id
+    WHERE ad.timestamp BETWEEN pb.start_time AND pb.end_time
+)
+
 #1873
 select employee_id , if((employee_id%2) = 1 and name not like 'M%', salary, 0) as bonus
 from Employees
